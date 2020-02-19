22ca1b6b33f246784c8548f393dba63be8decb88
webons.industries <germo.moeller@webons-industries.org>
2020-02-19 22:28:32 +0100

    0.4.4

--- rsrc/lib/webons.io/fx/PageConstructor/0.2.0_es5/index.js
+++ rsrc/lib/webons.io/fx/PageConstructor/0.2.0_es5/index.js
@@ -1,6 +1,4 @@
-'use strict';
-
-/*!
+'use strict';/*!
 *  formatrix 0.4.4 (http://formatrix.org)
 *  >> PageConstructor 0.2.0
 *
@@ -9,31 +7,5 @@
 *  This source code is licensed under the MIT license found in the
 *  LICENSE file in the root directory of this source tree.
 *
-*/ ///***************************************************************************>
-(function (fx) {
-  var $ = new fx(fx.vbind('lib://webons.io/fx/PageConstructor/', '0.2.0', '~?/'), { parent: 'sys://formatrix-0.4.4/', called: 'page' });$.prv = { $: { instance: null, config: { prefix: 'fx_' }, events: {} } };if (!Array.from) {
-    $.use({ polyfill: 'lib://webons.io/fx/PageConstructor/0.2.0/polyfill' });
-  }$.use({ evt: fx.vbind('lib://webons.io/fx/PageConstructor/') + 'events::generator', lib: fx.vbind('lib://webons.io/fx/PageConstructor/') + 'library' });$.pub({ get setup() {
-      if ($.prv.$.instance) {
-        return null;
-      } else {
-        return { start: function start(config) {
-            $.prv.$.config = $.fx.object_merge($.prv.$.config, config);$.prv.$.events = new $.evt({ singleton: true });$.prv.$.events.register('window::scroll', window, 'scroll');$.prv.$.events.register('window::resize', window, 'resize');$.fx.dom.ready(function () {
-              $.lib.init();
-            });$.fx.pointer.register($.fx.pointer.current($.$.id)['called'], $.lib.$.provided);$.prv.$.instance = false;
-          }, close: function close() {
-            $.prv.$.instance = true;
-          } };
-      }
-    }, $: { get prefix() {
-        return $.prv.$.config.prefix;
-      }, get events() {
-        return $.prv.$.events;
-      } }, get dom() {
-      return $.fx.dom;
-    }, get api() {
-      return $.lib.api;
-    }, get tools() {
-      return $.lib.tools;
-    } });
-})(Formatrix);
+*////***************************************************************************>
+(function(fx){var $=new fx(fx.vbind('lib://webons.io/fx/PageConstructor/','0.2.0','~?/'),{parent:'sys://formatrix-0.4.4/',called:'page'});$.prv={$:{instance:null,config:{prefix:'fx_'},events:{}}},Array.from||$.use({polyfill:'lib://webons.io/fx/PageConstructor/0.2.0/polyfill'}),$.use({evt:fx.vbind('lib://webons.io/fx/PageConstructor/')+'events::generator',lib:fx.vbind('lib://webons.io/fx/PageConstructor/')+'library'}),$.pub({get setup(){return $.prv.$.instance?null:{start:function start(config){$.prv.$.config=$.fx.object_merge($.prv.$.config,config),$.prv.$.events=new $.evt({singleton:!0}),$.prv.$.events.register('window::scroll',window,'scroll'),$.prv.$.events.register('window::resize',window,'resize'),$.fx.dom.ready(function(){$.lib.init()}),$.fx.pointer.register($.fx.pointer.current($.$.id).called,$.lib.$.provided),$.prv.$.instance=!1},close:function close(){$.prv.$.instance=!0}}},$:{get prefix(){return $.prv.$.config.prefix},get events(){return $.prv.$.events}},get dom(){return $.fx.dom},get api(){return $.lib.api},get tools(){return $.lib.tools}})})(Formatrix);
